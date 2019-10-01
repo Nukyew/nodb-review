a1b73fc480579d79958364322f261105267a8d4b
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-01 15:49:54 -0600

    add snipcart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,26 @@ export default function Header(props) {
         <header>
             <h1 onClick={() => audio.play()}>Pokemon Catcher 2000</h1>
             {/* <img src={props.image}/> */}
+            <button class="snipcart-add-item"
+                    data-item-name="My Subscription"
+                    data-item-id="subscription"
+                    data-item-url="https://thecarddrop.com/#/"
+                    data-item-price="20.00"
+                    data-item-payment-interval="Month"
+                    data-item-payment-interval-count="2"
+                    data-item-payment-trial="10">
+                    Subscribe now!
+                </button>
+                <button
+                    class="snipcart-add-item"
+                    data-item-id="2"
+                    data-item-name="Bacon"
+                    data-item-price="3.00"
+                    data-item-weight="20"
+                    data-item-url="https://thecarddrop.com/#/"
+                    data-item-description="Some fresh bacon">
+                    Buy bacon
+                </button>
         </header>
     )
 }
