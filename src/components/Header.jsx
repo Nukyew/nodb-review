0421546fc14df49cd9404f6eaf76f844dd89c718
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-01 18:26:43 -0600

    changed url of bacon again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header(props) {
                     data-item-id="2"
                     data-item-name="Bacon"
                     data-item-price="50.00"
-                    data-item-url="/api/products"
+                    data-item-url="https://thecarddrop.com/api/products"
                     data-item-description="Some fresh bacon">
                     Buy bacon
                 </button>
